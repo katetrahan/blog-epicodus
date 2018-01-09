45597e7d8a70c7af3e8378bf28145d6dc59b2ebc
Adam Calhoun and Kate Trahan <kate@>
2018-01-09 09:23:29 -0800

    initial set up, add tests, spark, handlebars

---
+++ src/main/java/models/Post.java
@@ -0,0 +1,29 @@
+package models;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+
+public class Post {
+    private String content;
+    private static ArrayList<Post> instances = new ArrayList<>();
+
+    public Post (String content){
+        this.content = content;
+        instances.add(this);
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public static ArrayList<Post> getAll(){
+        return instances;
+    }
+
+    public static void clearAllPosts(){
+        instances.clear();
+    }
+
+
+}
---
+++ src/test/java/models/PostTest.java
@@ -0,0 +1,41 @@
+package models;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PostTest {
+
+    @Test
+    public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals(true, post instanceof Post);
+    }
+
+    @Test
+    public void PostInstantiatesWithContent_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals("Day 1: Intro", post.getContent());
+
+    }
+
+    @Test
+    public void AllPostsContainsAllPosts_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post ("How to pair successfully");
+        assertTrue(Post.getAll().contains(post));
+        assertTrue(Post.getAll().contains(otherPost));
+    }
+
+    @Test
+    public void getAllContainsPosts_Integer()  {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post ("How to pair successfully");
+        assertTrue(Post.getAll().size() >= 2);
+    }
+
+
+}
