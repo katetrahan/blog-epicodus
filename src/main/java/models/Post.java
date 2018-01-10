ff78683b001cc8155225829e4b9b44be932f5a55
Danyel Murray and Kate Trahan <danyel@>
2018-01-10 14:02:59 -0800

    finish application

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,4 +1,5 @@
 package models;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -7,10 +8,19 @@ import java.util.Arrays;
 public class Post {
     private String content;
     private static ArrayList<Post> instances = new ArrayList<>();
+    private boolean published;
+    private LocalDateTime createdAt;
+    private int id;
+
+
 
     public Post (String content){
         this.content = content;
+        this.published = false;
+        this.createdAt = LocalDateTime.now();
         instances.add(this);
+        this.id = instances.size();
+        id = instances.size(); //I’m never null of zero. How come?
     }
 
     public String getContent() {
@@ -24,6 +34,34 @@ public class Post {
     public static void clearAllPosts(){
         instances.clear();
     }
+
+
+    public boolean getPublished() {
+        return this.published;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public static Post findById(int id){
+        return instances.get(id-1); //why minus 1? See if you can figure it out.
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void update(String content) {
+        this.content = content;
+    }
+
+    public void deletePost(){
+        instances.remove(id-1); //same reason
+    }
+
+
 
 
 }
